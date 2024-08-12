28848eefeda0a2a14d13e515bd9bb978522f0904
dev+1-at-029958659886 <miguelofoliveir>
2024-08-12 17:34:33 -0300

    feat: criar ListaRacasComponent como standalone

---
+++ app-racas-cachoros/src/app/componentes/lista-racas/lista-racas.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-racas',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      lista-racas works!
+    </p>
+  `,
+  styles: ``
+})
+export class ListaRacasComponent {
+
+}
